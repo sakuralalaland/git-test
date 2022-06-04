e988924eea1d9ef20802eaed92c768231aa37e6a
sakuralalaland <104971245+sakuralalaland@users.noreply.github.com>
2022-06-04 10:07:11 +0800

    four commit

--- src/main/java/myh/app1.java
+++ src/main/java/myh/app1.java
@@ -8,5 +8,6 @@ public class app1 {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("知晓天空之蓝的人啊！");
+        System.out.println("木叶飞舞之处，火意生生不息！");
     }
 }
