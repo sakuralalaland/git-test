252438a742fb8d03455fdd91caff2bc635058d21
lalaland <2818805974@qq.com>
2022-06-04 10:00:05 +0800

    third commit

--- src/main/java/myh/app1.java
+++ src/main/java/myh/app1.java
@@ -7,5 +7,6 @@ public class app1 {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("知晓天空之蓝的人啊！");
     }
 }
